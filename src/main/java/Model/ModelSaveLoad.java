8be2c28af9082498814788634575371be3e4039d
c-schuh <chano.schuh@gmail.com>
2023-07-03 10:55:50 +0200

    Methode für Speicherung hinzugefügt
    - Speicherung Methode
    - Bugfix filepath in getLeistungsabschnittFileNamen
    - Path in variable gespeichert

--- src/main/java/Model/ModelSaveLoad.java
+++ src/main/java/Model/ModelSaveLoad.java
@@ -6,6 +6,8 @@ package Model;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,14 +15,25 @@ import java.io.IOException;
  */
 public class ModelSaveLoad {
 
+    private final static String PATH ="./src/data/Leistungsabschnitte/";
+    /**
+     * deserialisere das searialisierte Leistungsabschnnitt-objekt "fileName" und gib es zurück
+     * 
+     * @param fileName Der Name der serialisierten Leistungsabschnitt Datei
+     * @return Das deserialisierte Leistungsabschnitt-Objekt
+     * @throws IOException Datei exisitiert nicht
+     * @throws ClassNotFoundException 
+     */
     public static Leistungsabschnitt ladeLeistungsabschnitt(String fileName) throws IOException, ClassNotFoundException{
-       return (Leistungsabschnitt)SerializationUtil.deserialize("./src/data/Leistungsabschnitte/" + fileName);
+       return (Leistungsabschnitt)SerializationUtil.deserialize(PATH + fileName);
     }
-
+    /**
+     * gebe alle Namen der Dateien im Leistungsabschnitt-directory zurück
+     * 
+     * @return Alle Dateinamen im Speicherdirectory in einem Array
+     */
     public static String[] getLeistungsabschnittFileNamen() {
-
-        File[] files = new File("/path/to/the/directory").listFiles();
-
+        File[] files = new File(PATH).listFiles();
         String[] ergebnisse = new String[files.length];
 
         for (int i = 0; i < files.length; i++) {
@@ -29,5 +42,18 @@ public class ModelSaveLoad {
             }
         }
         return ergebnisse;
+    }
+    /**
+     * serialisiere das Leistungsabschnitt-Objekt toSave und speichere dieses als Datei mit dem Namen des Leistungsabschnitts
+     * 
+     * @param toSave Das Leistungsabschnitt-Objekt das gepsiechert werden soll
+     */
+    public static void speicherLeistungsabschnitt(Leistungsabschnitt toSave){
+        String fileName = toSave.getName() + ".ser";
+        try {
+            SerializationUtil.serialize(toSave, PATH + fileName);
+        } catch (IOException ex) {
+            Logger.getLogger(ModelSaveLoad.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
