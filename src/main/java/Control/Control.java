b250a0e975a9d47ba38e55bed7787944b89bbfee
simon <s.noess214@gymhogau.de>
2023-07-06 23:09:45 +0200

    Versuch die grundlegenden Funktionen des Controllers hinzuzufügen. Fragen aber noch offen.

--- src/main/java/Control/Control.java
+++ src/main/java/Control/Control.java
@@ -4,14 +4,44 @@
  */
 package Control;
 
+import View.View;
+import Model.Fach;
+import Model.Leistungsabschnitt;
+import Model.LeistungsnachweisArt;
+import Model.NotenArt;
+import Model.NotenElement;
+
 /**
  *
- * @author Voxiell
+ * @author Voxiell, simon
  */
-public class Control {
-    
+public class Control { 
+    // Startup Initialisiert das Programm mit Model und View. Mach des Sinn?
    public void startup () {
+      View1=new View(this);
+      Model1 = new Model();
+      View1.setModel(Model1);
+   }
+   
+   //fügt neuen Leistungsabschnitt hinzu
+   public boolean Leistungsabschnitthinzufügen (String name){
+       new Leistungsabschnitt(name);
+       return true;
        
    }
+   
+   //fügt Fach zu Leistungsabschnitt hinzu
+   public boolean Fachhinzufügen (String name, int Leistungsabschnitt){
+      Leistungsabschnitt.addFach(new Fach(name));
+       
+       return true;
+       
+   }
+   
+   public boolean Notehinzufügen (String name, String Fach, int Notenwert){
+       
+       
+   }
+       
     
 }
